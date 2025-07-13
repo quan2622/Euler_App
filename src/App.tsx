f4f08ec85075ea0cce4d245fa49e8ec7c7597605
Quan Nguyen Hong <155934975+quan2622@users.noreply.github.com>
2025-07-13 12:56:55 +0700

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-400">
         Hello world!
       </h1>
+      <Button variant={"outline"} onClick={() => alert("Button had been click")}>Click me!</Button>
     </>
   )
 }
