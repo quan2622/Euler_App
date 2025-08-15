6daca9ecfb1fd3cf5702bab3700548440b1027b7
Quan Nguyen Hong <155934975+quan2622@users.noreply.github.com>
2025-08-16 00:08:39 +0700

    - config hide menu bar - config short key for reload app & dev tools

--- dist-electron/main.cjs
+++ dist-electron/main.cjs
@@ -7,6 +7,45 @@ const windowManager_1 = require("./windowManager.cjs");
 let mainWindow = null;
 electron_1.app.whenReady().then(() => {
     mainWindow = (0, windowManager_1.createMainWindow)();
+
+    // Khởi đầu cấu hình menu
+    const menuTemplate = [
+        {
+            label: 'File',
+            submenu: [
+                { role: 'quit', accelerator: 'CmdOrCtrl+Q' }
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                {
+                    label: 'Reload',
+                    accelerator: 'F5', // Thay đổi phím tắt thành F5
+                    click: (menuItem, browserWindow) => {
+                        if (browserWindow) {
+                            browserWindow.reload();
+                        }
+                    }
+                },
+                { role: 'forceReload' },
+                { type: 'separator' },
+                {
+                    label: 'Toggle Developer Tools',
+                    accelerator: 'F12', // Thay đổi phím tắt thành F12
+                    click: (menuItem, browserWindow) => {
+                        if (browserWindow) {
+                            browserWindow.webContents.toggleDevTools();
+                        }
+                    }
+                }
+            ]
+        }
+    ];
+
+    const menu = electron_1.Menu.buildFromTemplate(menuTemplate);
+    electron_1.Menu.setApplicationMenu(menu);
+
     if (process.env.NODE_ENV === 'development') {
         mainWindow.webContents.openDevTools();
     }
